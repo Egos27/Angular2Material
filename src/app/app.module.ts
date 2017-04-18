35912ccb5a3b9b43f2bc1e9423b587215869cfb3
Ezequiel Oggioni <eoggioni@aga-archivos.com.ar>
2017-04-18 10:46:07 -0300

    Agrego la libreria  e Importo la clase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+//1 Agrego la libreria
+import {MaterialModule} from '@angular/material';
 
-// 1- incluir el modulo para 
 import {RouterModule,Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
-//2- esto debe estar y e auto genera
+
 import { LoginComponent } from './login/login.component';
 import { PaginaNoEncontradaComponent } from './pagina-no-encontrada/pagina-no-encontrada.component';
-//3 creo el array para el ruteo
+
 const rutasDenavegacion:Routes =[{path:'login',component:LoginComponent},
 {path: '',redirectTo :'/login' ,pathMatch:'full'},
 {path:'**',component: PaginaNoEncontradaComponent}];
@@ -24,9 +25,10 @@ const rutasDenavegacion:Routes =[{path:'login',component:LoginComponent},
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    // 4-importamos el moduflo con las rutas
-    RouterModule.forRoot(rutasDenavegacion)
+    HttpModule,    
+    RouterModule.forRoot(rutasDenavegacion),
+    //2 Importo la clase
+    MaterialModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
